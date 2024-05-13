5d24a5f419bfbad563ec1a4bf5bd9ee95d571c75
priyaK9157 <priyakumari2003004@gmail.com>
2024-05-13 18:49:40 +0530

    common navbar commit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from '../components/commonPage/Navbar'
 import 'tailwindcss/tailwind.css';
+import HomePage from "../components/pages/HomePage"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      
+      <HomePage/>
     </div>
   );
 };
--- client/src/components/commonPage/Navbar.tsx
+++ client/src/components/commonPage/Navbar.tsx
@@ -1,35 +1,51 @@
 import Link from 'next/link';
+import { BsBagDashFill } from "react-icons/bs";
+import { IoIosSearch } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-200 border h-8 flex justify-center text-gray-700">
-      <ul className="flex flex-row gap-6 items-center">
-        <li className="">
-          <Link href="/">
-            <span className="">Home</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/find-job">
-            <span className="">Find Job</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/employers">
-            <span className="">Employers</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/candidates">
-            <span className="">Candidates</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/customer-support">
-            <span className="">Customer Support</span>
-          </Link>
-        </li>
-      </ul>
+    <nav className=" flex flex-col gap-4 text-gray-700 items-center">
+     
+        
+            <ul className="flex flex-row gap-6 items-center justify-center h-10 bg-gray-200 w-full ">
+                <li className="">
+                <Link href="/">
+                    <span className="">Home</span>
+                </Link>
+                </li>
+                <li className="">
+                <Link href="/find-job">
+                    <span className="">Find Job</span>
+                </Link>
+                </li>
+                <li className="">
+                <Link href="/employers">
+                    <span className="">Employers</span>
+                </Link>
+                </li>
+                <li className="">
+                <Link href="/candidates">
+                    <span className="">Candidates</span>
+                </Link>
+                </li>
+                <li className="">
+                <Link href="/customer-support">
+                    <span className="">Customer Support</span>
+                </Link>
+                </li>
+            </ul>
+      
+
+        <div className='flex gap-10'>
+            <div className='flex items-center gap-3'>
+                <BsBagDashFill style={{color: '#7D0DC3' , fontSize: '24px'}}/>
+                <p className='font-bold text-2xl'>Projectpilot</p>
+            </div>
+            <div className='flex items-center border border-gray-400 rounded-md gap-3 p-3'>
+                <IoIosSearch style={{ fontSize: '24px' }} />
+                <input placeholder='Search for Projects'/>
+            </div>
+        </div>
     </nav>
   );
 };

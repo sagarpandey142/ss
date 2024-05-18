b7388cb8a18588ea01e9693c608f47afd5404c9f
priyaK9157 <priyakumari2003004@gmail.com>
2024-05-18 10:50:57 +0530

    Profile Page commit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,26 @@
+'use client'
+
 import Navbar from '../components/commonPage/Navbar'
 import 'tailwindcss/tailwind.css';
 import HomePage from "../components/pages/HomePage"
+import ResetPassword from "../components/pages/ResetPassword"
+import SignIn from '../components/pages/SignIn'
+import SignUp from "../components/pages/SignUp"
+import RecoverAcc from "../components/pages/RecoverAcc"
+import OtpPage from "../components/pages/OtpPage"
+import CreateProfile from '@/components/pages/CreateProfile';
+import DashboardPage from '@/components/pages/DashboardPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <HomePage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/otp" element={<OtpPage />} />
+      </Routes>
+    </Router>
   );
 };
 

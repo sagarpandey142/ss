d0f7e0239e293ceea652d9f51c4dc7243058a0d7
priyaK9157 <priyakumari2003004@gmail.com>
2024-05-19 11:02:36 +0530

    Merge branch 'Chan_FolderStart' into main

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 // Import necessary components and hooks from Next.js
-import Navbar from '../components/commonPage/Navbar';
+import Navbar from '../app/components/commonPage/Navbar';
 import 'tailwindcss/tailwind.css';
-import HomePage from "../components/pages/HomePage";
-import ResetPassword from "../components/pages/ResetPassword";
-import SignIn from '../components/pages/SignIn';
-import SignUp from "../components/pages/SignUp";
-import RecoverAcc from "../components/pages/RecoverAcc";
-import OtpPage from "../components/pages/OtpPage";
-import CreateProfile from '@/components/pages/CreateProfile';
-import DashboardPage from '@/components/pages/DashboardPage';
+import HomePage from "../app/components/pages/HomePage";
+import ResetPassword from "../app/components/pages/ResetPassword";
+import SignIn from '../app/components/pages/SignIn';
+import SignUp from "../app/components/pages/SignUp";
+import RecoverAcc from "../app/components/pages/RecoverAcc";
+import OtpPage from "../app/components/pages/OtpPage";
+import CreateProfile from '@/app/components/pages/CreateProfile';
+import DashboardPage from '@/app/components/pages/DashboardPage';
 import { usePathname } from 'next/navigation';
 
 
